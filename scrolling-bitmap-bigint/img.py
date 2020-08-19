055e9948b31bb151ea28cb4dbd27024dc99166b3
Alon Gal <alon.gal@khealth.ai>
2020-08-19 11:32:17 +0300

    Bigint bitmap scroller version

--- scrolling-bitmap-bigint/img.py
+++ scrolling-bitmap-bigint/img.py
@@ -3,20 +3,17 @@ This script converts a png image into a dweet friendly hex
 """
 import imageio
 
-image_path = "scrolling-bitmap/image.png"
+image_path = "scrolling-bitmap-bigint/image.png"
 image = imageio.imread(image_path)
-# It's actually a half-byte
 byte = ""
 
-for row in image:
-    for col in row:
+print('0x',end='')
+for row in image[::-1]:
+    for col in row[::-1]:
         byte += "1" if col[0] == 0 else "0"
         if len(byte) == 4:
-            # Reverse the bits in the byte
-            byte = byte[::-1]
-
-            # Print the bytes as hex
+            # Print as hex
             i = int(byte, 2)
             print(hex(i)[2:], end="")
             byte = ""
-print()
+print('n')
